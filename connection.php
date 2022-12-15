b59f9992286d3a1c211447d106c61ca2ceeb2b6a
gdr-00 <gabriele.dare00@gmail.com>
2022-12-15 13:25:17 +0100

    added getlist on php

--- connection.php
+++ connection.php
@@ -11,10 +11,32 @@ class DBAccess{
     private $connection;
 
     public function openDBConnection() {
+
+        mysqli_report(MYSQLI_REPORT_ERROR);
+
         $this->connection = mysqli_connection(
             DBAccess::HOST_DB, DBAccess::USERNAME,
             DBAccess::PASSWORD, DBAccess::DATABASE_NAME
         );
+
+        if(mysqli_connect_errno()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public function getList(){
+        $query = "SELECT * FROM giocatori ORDER BY ID ASC";
+
+        $queryResult = mysqli_query($this->connection, $query) or 
+        die("Errore in openDBConnection: ".mysqli_error($this->connection));
+
+        if(mysqli_num_rows($queryResult)==0){
+            return NULL;
+        } else {
+            
+        }
     }
 }
 
